2ce387646a56ff1a6094aaaec92790555b9518de
Lorenzo Romagnoni <r.lorenzo1810@gmail.com>
2023-04-07 22:27:45 +0200

    chore: cleaning implementation

--- src/core/dispatchAction.ts
+++
@@ -1,8 +0,0 @@
-export type DispatchAction<Tag extends string, Payload = never> = {
-  tag: Tag;
-  payload?: Payload;
-};
-
-export function action<Tag extends string>(tag: Tag): DispatchAction<Tag> {
-  return { tag };
-}
